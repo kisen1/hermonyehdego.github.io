8e244fd744efed13cda23c92d65295a0071527e4
hermon.yehdego <hermon.yehdego@miu.edy>
2023-03-04 09:32:53 -0800

    iuyt

---
+++ TaskList/task.js
@@ -0,0 +1,29 @@
+let allTasks = localStorage.getItem("taskME");
+if (allTasks === null) {
+    allTasks = "";
+}
+
+function addTask() {
+    if (typeof (Storage) !== undefined) {
+        let holdTask = document.getElementById("task").value;
+        allTasks = holdTask + " \n" + allTasks;
+
+        document.getElementById("taskList").value = allTasks;
+        document.getElementById("task").value = "";
+        localStorage.setItem("taskME", allTasks);
+    }
+    else {
+        document.getElementById("taskList").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}
+function clearTask() {
+    document.getElementById("taskList").value = "";
+    document.getElementById("task").value = "";
+    allTasks = "";
+
+
+    localStorage.removeItem("taskME");
+}
+window.onload = function () {
+    document.getElementById("taskList").value = allTasks;
+};
