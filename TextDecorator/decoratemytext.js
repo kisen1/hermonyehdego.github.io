43b1785ca4ae312b620570f07ac1c3b766fb83ea
hermon.yehdego <hermon.yehdego@miu.edy>
2023-03-06 13:52:44 -0800

    uiop

--- TextDecorator/decoratemytext.js
+++ TextDecorator/decoratemytext.js
@@ -1,17 +1,33 @@
+window.onload = function () {
+    document.getElementById("area").innerHTML = "\t\t"+"Sample Item 1"+"\r\n"+"\t\t"+"Sample Item 2";
+    var click = document.getElementById("big");
+    click.onclick = bigClick;
+   
+};
+function big(){
 
-
-
-var urlMenu= document.getElementById('changeEx');
-
-
-urlMenu.onchange=function(){
-
-
-    var useroption = this.option[this.selectedIndex];
-
-    if(useroption.value!="nothing"){
-        windom.open(useroption.value)
-    }
-
-
+  var text = document.getElementById("area");
+  var font = window.getComputedStyle(text);
+  var size = font.fontSize;
+  var fontsize = parseInt(size)+2+"px";
+  document.getElementById("area").style.fontSize=fontsize;
+  console.log(fontsize);
 }
+function bigClick(){setInterval(big,300);}
+
+
+function change(e){
+
+   if(e.checked){
+  
+    document.getElementById("area").style.fontWeight="bold";
+    document.getElementById("area").style.color="green";
+    document.getElementById("area").style.textDecoration="underline";
+   }
+   if(!e.checked){
+   
+    document.getElementById("area").style.fontWeight="normal";
+    document.getElementById("area").style.color="black";
+    document.getElementById("area").style.textDecoration="none";
+   }
+} 
