7df47329b721b84310efc4857838c9fd603f8828
hermon.yehdego <hermon.yehdego@miu.edy>
2023-03-17 14:32:07 -0700

    kjuio

--- NumberQuizeLab/numberquizeserver.js
+++ NumberQuizeLab/numberquizeserver.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var output = require('./numberquize.js');
 
 http.createServer(function (req, res) {
-    var q = new URL(req.url, "https://localhost:8080/");
+    var q = new URL(req.url, "http://localhost:8080");
     output.guessnumber(req, res, q.searchParams);
   })
   .listen(8080);
