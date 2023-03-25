0c6eb396d241c9047c4c1ec10a7f868e533873eb
hermo <hermon.yehdego@miu.edu>
2023-03-25 17:41:44 -0500

    last

--- lab-12/calculatormod.js
+++ lab-12/calculatormod.js
@@ -19,7 +19,7 @@ exports.displays=function(req,res,vals){
     res.write("<title>Calculator Web Site</title>"); 
     res.write("</head>");
     res.write("<body>");
-    res.write("<p style=\"background-color:bisque\" ; style=\"font:14pt bold\";>The result is: ");
+    res.write("<p  ; style=\"font:14pt bold\";>The result is: ");
           res.write(String(result));
     res.write("</p>");
     res.write(
